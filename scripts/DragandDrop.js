5970e0668e9f404e67cbb21392cafc32a825c01b
Bendikl96 <bendik.luthentun@hotmail.com>
2020-11-02 15:30:09 +0100

    yup

--- scripts/DragandDrop.js
+++ scripts/DragandDrop.js
@@ -1,17 +1,17 @@
 
 
-function allowDrop(ev) {
-    ev.preventDefault();
+function allowDrop(event) {
+    event.preventDefault();
   }
   
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+  function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
   }
   
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    const dropzone = ev.target;
+  function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+    const dropzone = event.target;
     dropzone.appendChild(draggableElement);
   }
